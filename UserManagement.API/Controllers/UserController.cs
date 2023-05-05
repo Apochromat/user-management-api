2c76cfde372794358c00a970b06fb954fa88abac
Stepan Ustimenko <50939552+Apochromat@users.noreply.github.com>
2023-05-05 17:53:16 +0700

    Base project

---
+++ UserManagement.API/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserManagement.API.Controllers;
+
+/// <summary>
+/// Controller for managing users
+/// </summary>
+[ApiController]
+[Route("api")]
+public class UserController : ControllerBase {
+    private readonly ILogger<UserController> _logger;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="logger"></param>
+    public UserController(ILogger<UserController> logger) {
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Creates a new user
+    /// </summary>
+    /// <returns></returns>
+    [HttpPost]
+    [Route("user")]
+    public async Task<ActionResult> CreateUser() {
+        return Ok();
+    }
+    
+    /// <summary>
+    /// Returns all users with pagination
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    [Route("users")]
+    public async Task<ActionResult> GetAllUsers() {
+        return Ok();
+    }
+    
+    /// <summary>
+    /// Returns a single user
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    [Route("user/{id}")]
+    public async Task<ActionResult> GetUser() {
+        return Ok();
+    }
+
+    /// <summary>
+    /// Deletes a user
+    /// </summary>
+    /// <returns></returns>
+    [HttpDelete]
+    [Route("user/{id}")]
+    public async Task<ActionResult> DeleteUser() {
+        return Ok();
+    }
+}
